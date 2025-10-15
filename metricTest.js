6bad8603e084fbe640f8efc15026e77ede81c574
ArazAhmet <aa227xv@student.lnu.se>
2025-10-15 16:04:26 +0200

    create a test file to check if the functionality works

---
+++ metricTest.js
@@ -0,0 +1,4 @@
+import { analyzeRepository } from './node_modules/code-metric-analyzer/codeMetric.js'
+
+// Analyze specific repository
+await analyzeRepository('https://github.com/ArazAhmet/L2-1dv610.git')
